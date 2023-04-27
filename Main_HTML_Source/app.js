60cf204acf9c7b698a1e759de9b70c91c42f891d
jordan207 <129029441+jordan207@users.noreply.github.com>
2023-04-27 15:25:30 +0800

    Update app.js

--- Main_HTML_Source/app.js
+++ Main_HTML_Source/app.js
@@ -1,7 +1,7 @@
 URL = window.URL || window.webkitURL;
 const jsondata = {
 	age: 0,
-    education_yrs: 0,
+    education_yrs: 1,
     cigsPerDay: 0,
     glucose: 0,
     totChol: 0,
@@ -15,134 +15,6 @@ const jsondata = {
     sysBP: false,
     diaBP: false,
 	gender: 'male',
-}
-
-let recordings = {
-	"breathing-shallow": null,
-	"breathing-deep": null,
-	"cough-shallow": null,
-	"cough-heavy": null,
-	"vowel-a": null,
-	"vowel-e": null,
-	"vowel-o": null,
-	"counting-normal": null,
-	"counting-fast": null,
-};
-// object key recordings
-
-let recordingsKey = Object.keys(recordings);
-let gumStream; 						//stream from getUserMedia()
-let rec; 							//Recorder.js object
-let input; 							//MediaStreamAudioSourceNode we'll be recording
-let coughHeavyBlob;
-// shim for AudioContext when it's not avb. 
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-let audioContext //audio context to help us record
-
-
-function startRecording(audio_name, index, hint) {
-	console.log("recordButton clicked");
-
-	/*
-		Simple constraints object, for more advanced audio features see
-		https://addpipe.com/blog/audio-constraints-getusermedia/
-	*/
-
-	const constraints = { audio: true, video: false }
-
-	/*
-	  Disable the record button until we get a success or fail from getUserMedia() 
-  */
-
-
-	/*
-		We're using the standard promise based getUserMedia() 
-		https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-	*/
-
-	navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-
-		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
-		$(`#${audio_name}-controls`).empty();
-		$(`#${audio_name}-controls`).append(`
-			<p>${hint}</p>
-			<button class="btn btn-primary" id="stop-${audio_name}" onclick="stopRecording('${audio_name}', ${index}, '${hint}')">Stop Recording</button>
-		`)
-		/*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
-		*/
-		audioContext = new AudioContext();
-
-		/*  assign to gumStream for later use  */
-		gumStream = stream;
-
-		/* use the stream */
-		input = audioContext.createMediaStreamSource(stream);
-
-		/* 
-			Create the Recorder object and configure to record mono sound (1 channel)
-			Recording 2 channels  will double the file size
-		*/
-		rec = new Recorder(input, { numChannels: 1 })
-
-		//start the recording process
-		rec.record()
-
-
-		console.log("Recording started");
-
-	}).catch(function (err) {
-		//enable the record button if getUserMedia() fails
-	});
-}
-
-
-function stopRecording(audio_name, index, hint) {
-	console.log("stopButton clicked");
-	//reset button just in case the recording is stopped while paused
-
-	//tell the recorder to stop the recording
-	rec.stop();
-
-	//stop microphone access
-	gumStream.getAudioTracks()[0].stop();
-
-	rec.exportWAV(function (blob) {
-		coughHeavyBlob = blob;
-		recordings[audio_name] = blob;
-		const url = URL.createObjectURL(blob);
-		$(`#${audio_name}`).attr("src", url);
-	});
-	$(`#${audio_name}-controls`).empty();
-	$(`#${audio_name}-controls`).append(`
-		<p>${hint}</p>
-        <button class="btn btn-primary" id="playRecordedAudio" onclick="playRecordedAudio('${audio_name}')">Play</button>
-        <button id="recordButton"class="btn btn-primary" onclick="saveAudio('${audio_name}', ${index + 1})">Save</button>
-        <button id="recordButton"class="btn btn-primary" onclick="goRecord('${audio_name}', ${index}, '${hint}')">Record Again</button>
-    `)
-}
-function saveAudio(audio_name, nextAudioIndex) {
-	console.log(recordings)
-	$(`#${audio_name}-title`).addClass("text-success");
-
-	$(`#${audio_name}-controls`).hide();
-	$(`#${recordingsKey[nextAudioIndex]}-controls`).show();
-}
-
-function goRecord(audio_name, index, hint) {
-	$(`#${audio_name}-controls`).empty();
-	$(`#${audio_name}-controls`).append(`
-	<p>${hint}</p>
-    <button id="recordButton"class="btn btn-primary" onclick="startRecording('${audio_name}', ${index}, '${hint}')">Start Record</button>
-    <button id="playSampleButton"class="btn btn-primary" onclick="playSample('sample-${audio_name}')">Play Sample</button>
-`);
-}
-
-function playRecordedAudio(audio_name) {
-	const audio = new Audio($(`#${audio_name}`).attr("src"));
-	audio.play();
 }
 
 function dataChange(inputname) {
@@ -220,12 +92,7 @@ function submit() {
 		}
 		loader.addClass('done');
 	};
-	// xhr.open("POST", "https://api.leadinghao.me/covid_detection", true);
+	// xhr.open("POST", "https://api.imjordan.me/predict", true);
 	xhr.open("POST", "http://localhost:8000/predict", true);
 	xhr.send(formData);
-}
-
-function playSample(sampleHTMLId) {
-	const audio = new Audio($(`#${sampleHTMLId}`).attr("src"));
-	audio.play();
 }
